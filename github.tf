01ac7b04ac55c9fd048ee4999db1b03543c557e8
kathe-ud <160063719+kathe-ud@users.noreply.github.com>
2024-02-14 16:21:26 -0500

    Update github.tf

--- github.tf
+++ github.tf
@@ -10,19 +10,17 @@ terraform {
 # Configure the GitHub Provider
 provider "github" {
   token = var.github_token
+  organization = "BUILDING BLOCK TSS"
+  team         = "Team Gus"
+  owners = [
+    "kathe-ud",
+  ]
 }
 
 resource "github_repository" "main" {
   name         = "jelpi-mf"
   description  = "Mi repositorio de prueba"
   visibility   = "private"
-  organization = "BUILDING BLOCK TSS"
-  team         = "Team Gus"
-  project_type = "private"
-
-
-  owners = [
-    "kathe-ud",
-  ]
+  project_type = "private" 
 
 }
