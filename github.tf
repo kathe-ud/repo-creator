6f280f02162cd7efc0a09bf04d7a3cbff71436f7
kathe-ud <160063719+kathe-ud@users.noreply.github.com>
2024-02-14 14:39:23 -0500

    Update github.tf

--- github.tf
+++ github.tf
@@ -9,7 +9,7 @@ terraform {
 
 # Configure the GitHub Provider
 provider "github" {
-  token = "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"
+  token = "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"
 }
 
 resource "github_repository" "example" {
