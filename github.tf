1c547791bf7b2ee93ac01c524f830fcc2b627aa9
kathe-ud <160063719+kathe-ud@users.noreply.github.com>
2024-02-15 20:25:34 -0500

    Update github.tf

--- github.tf
+++ github.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     github = {
       source  = "integrations/github"
-      version = "6.0.0-rc2"
+      version = "~> 5.0"
     }
   }
 }
@@ -17,13 +17,4 @@ resource "github_repository" "jelpi-mf" {
   name         = "jelpi-mf2"
   description  = "Mi repositorio de prueba"
   visibility   = "private"
-}
-
-resource "github_repository_collaborators" "some_repo_collaborators" {
-  repository = "jelpi-mf2"
-
-  team {
-    permission = "pull"
-    team_id = "team-gus"
-  }
 }
