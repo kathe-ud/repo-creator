db71d4d9b016f51aead0316b5fb03e06d5b14712
kathe-ud <160063719+kathe-ud@users.noreply.github.com>
2024-02-15 21:44:46 -0500

    Update github.tf

--- github.tf
+++ github.tf
@@ -10,11 +10,11 @@ terraform {
 # Configure the GitHub Provider
 provider "github" {
   token = var.github_token
-  owner = "kathe-ud"
+  owner = "BUILDING-BLOCK-TSS"
 }
 
-resource "github_repository" "jelpi-mf" {
-  name         = "jelpi-mf2"
+resource "github_repository" "example" {
+  name         = "example"
   description  = "Mi repositorio de prueba"
   visibility   = "private"
 }
