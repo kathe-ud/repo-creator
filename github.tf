0f07a23a31ee26c151fb596939a974200504fa12
kathe-ud <160063719+kathe-ud@users.noreply.github.com>
2024-02-21 16:02:50 -0500

    Update github.tf

--- github.tf
+++ github.tf
@@ -13,11 +13,6 @@ provider "github" {
   owner = "BUILDING-BLOCK-TSS"
 }
 
-data "github_team" "mi_equipo" {
-  owner = "BUILDING-BLOCK-TSS"
-  slug  = "team-gus"
-}
-
 resource "github_repository" "example" {
   name         = "example4"
   description  = "Mi repositorio de prueba"
@@ -29,3 +24,7 @@ resource "github_team_repository" "some_team_repo" {
   repository = "example4"
   permission = "pull"
 }
+
+output "repo_name" {
+    value = github_repository.example.name
+  }
